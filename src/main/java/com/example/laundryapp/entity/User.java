e7e6df9ff7f75528828107f24d81ef2fc0a9022f
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-05-15 09:27:24 -0500

    added user entity

---
+++ src/main/java/com/example/laundryapp/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.laundryapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
