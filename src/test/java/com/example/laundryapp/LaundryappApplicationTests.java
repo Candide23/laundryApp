f1f8fcdb0719a8d292d1fcc847356b550312a5e6
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-05-15 08:27:31 -0500

    first commit

---
+++ src/test/java/com/example/laundryapp/LaundryappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laundryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaundryappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
