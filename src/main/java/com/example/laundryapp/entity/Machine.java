c0e74c33b7992b42cda7ca0ab1d0bcd854d8bfd5
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-05-16 08:05:06 -0500

    added entity machine

---
+++ src/main/java/com/example/laundryapp/entity/Machine.java
@@ -0,0 +1,26 @@
+package com.example.laundryapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "machines")
+public class Machine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column(nullable = false)
+    private String status;
+}
