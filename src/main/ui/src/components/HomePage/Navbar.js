25a7782174c1f303923d3c726d57ebf90c870864
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-05-21 08:38:31 -0500

    updated navbar components

--- src/main/ui/src/components/HomePage/Navbar.js
+++ src/main/ui/src/components/HomePage/Navbar.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import logoImage from '../../images/your-logo.png'; // Ensure the path to your logo is correct
 
-
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
       <div className="container-fluid">
         <a className="navbar-brand" href="#home">
-        <img src={logoImage} alt="LaundryApp Logo" style={{ height: '100px' }} />
-          
+          <img src={logoImage} alt="LaundryApp Logo" style={{ height: '30px' }} />
+          LaundryApp
         </a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -16,7 +15,7 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page"  href="#home">Home</a>
+              <a className="nav-link active" aria-current="page" href="#home">Home</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#features">Features</a>
@@ -35,5 +34,6 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
 
 
