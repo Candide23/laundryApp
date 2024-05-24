604adcd7e71624b3fa10ccc6974caaffd09f9700
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-05-24 08:33:03 -0500

    added createMachine service

--- src/main/java/com/example/laundryapp/service/MachineService.java
+++ src/main/java/com/example/laundryapp/service/MachineService.java
@@ -1,4 +1,8 @@
 package com.example.laundryapp.service;
 
+import com.example.laundryapp.dto.MachineDto;
+
 public interface MachineService {
+
+    MachineDto createMachine(MachineDto machineDto);
 }
--- src/main/java/com/example/laundryapp/service/impl/MachineServiceImpl.java
+++ src/main/java/com/example/laundryapp/service/impl/MachineServiceImpl.java
@@ -1,4 +1,25 @@
 package com.example.laundryapp.service.impl;
 
-public class MachineServiceImpl {
+import com.example.laundryapp.dto.MachineDto;
+import com.example.laundryapp.entity.Machine;
+import com.example.laundryapp.mapper.MachineMapper;
+import com.example.laundryapp.repository.MachineRepository;
+import com.example.laundryapp.service.MachineService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class MachineServiceImpl implements MachineService {
+
+    private MachineRepository machineRepository;
+
+
+    @Override
+    public MachineDto createMachine(MachineDto machineDto) {
+        Machine machine = MachineMapper.mapToMachine(machineDto);
+        Machine saveMachine = machineRepository.save(machine);
+
+        return MachineMapper.mapToMachineDto(saveMachine);
+    }
 }
