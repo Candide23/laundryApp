64bed41ad3aa0dfa69fbe017fe13791c4b02f5d3
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-06-03 13:19:30 -0500

    added findByAddress function

--- src/main/java/com/example/laundryapp/repository/MachineRepository.java
+++ src/main/java/com/example/laundryapp/repository/MachineRepository.java
@@ -2,6 +2,11 @@ package com.example.laundryapp.repository;
 
 import com.example.laundryapp.entity.Machine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface MachineRepository extends JpaRepository<Machine, Long> {
+    List<Machine> findByAddress(String address);
+
 }
